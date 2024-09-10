720e2b415e6a622faae7c4203c3474d2186d195d
David Almonacid <david.almonacidf@gmail.com>
2024-09-10 07:11:53 -0500

    Rename Daypart enum class

--- src/PracticeClassesAndCollections.kt
+++ src/PracticeClassesAndCollections.kt
@@ -4,11 +4,11 @@
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: DayPart,
+    val daypart: Daypart,
     val durationInMinutes: Int,
 )
 
-enum class DayPart {
+enum class Daypart {
     MORNING, AFTERNOON, EVENING
 }
 
@@ -16,7 +16,7 @@ fun main() {
     val myEvent = Event(
         title = "Study Kotlin",
         description = "Commit to studying Kotlin at least 15 minutes per day.",
-        daypart = DayPart.EVENING,
+        daypart = Daypart.EVENING,
         durationInMinutes = 15,
     )
     println(myEvent)
